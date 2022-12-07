bc923942e8e3162e8d590128eb318aad037042c9
Illia Zhdanov <il.25.zhd@gmail.com>
2022-12-07 23:40:39 +0200

    1.0

--- src/test/java/me/illia/jlog/demo/JLogDemo.java
+++ src/test/java/me/illia/jlog/demo/JLogDemo.java
@@ -3,9 +3,14 @@ package me.illia.jlog.demo;
 import me.illia.jlog.*;
 
 public class JLogDemo {
+    // The JLoggerStyle() Without any args just creates a default style, but with args, just put in the ansi colors as args :)
+    // That's why the args for that are Strings
     public static final JLoggerStyle style = new JLoggerStyle();
     public static void main(String[] args) {
+        // Set the JLogger style
         JLogger.setStyle(style);
-        JLogger.log(JLogLvl.NORMAL, "Hello JLog!");
+
+        // Print out 'Hello JLog!' yellow
+        JLogger.log(JLogLvl.WARNING, "Hello JLog!");
     }
 }
