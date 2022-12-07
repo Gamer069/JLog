cfe684ffa913ad690886d58a52b1bfaf93f90f23
Illia Zhdanov <il.25.zhd@gmail.com>
2022-12-07 23:32:55 +0200

    Initial Commit

--- src/test/java/me/illia/jlog/demo/JLogDemo.java
+++ src/test/java/me/illia/jlog/demo/JLogDemo.java
@@ -5,5 +5,7 @@ import me.illia.jlog.*;
 public class JLogDemo {
     public static final JLoggerStyle style = new JLoggerStyle();
     public static void main(String[] args) {
+        JLogger.setStyle(style);
+        JLogger.log(JLogLvl.NORMAL, "Hello JLog!");
     }
 }
