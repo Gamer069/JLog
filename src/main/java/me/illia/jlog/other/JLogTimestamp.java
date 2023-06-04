c008931966fdb9970ac215057662bd9f9ab1f677
Illia Zhdanov <106342228+Gamer069@users.noreply.github.com>
2023-06-04 13:28:33 +0300

    Update JLogTimestamp.java to have a better formatted time. (Now instead of H_M_S, it's now H:M:S

--- src/main/java/me/illia/jlog/other/JLogTimestamp.java
+++ src/main/java/me/illia/jlog/other/JLogTimestamp.java
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class JLogTimestamp {
-    private static final SimpleDateFormat format = new SimpleDateFormat("HH_mm_ss");
+    private static final SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
 
     public static String formatted(Date date) {
         return format.format(date);
